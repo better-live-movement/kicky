f8ae25380a98946acafaaa6e89270a8c1231c81b
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2018-01-25 01:52:44 +0100

    update support link, help and infocard

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- To make it easier for new users, we set the `help` command as status.
 
 ## [2.0.1] - 2017-11-28
 ### Fixed
--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const  Discord = require('discord.js');
 const TOKEN = require('./token.js');
 const PREFIX = 'd!';
 const VERSION = '2.0.0';
-const GITHUB = 'https://github.com/mjkatgithub/d-bot'
+const SUPPORT = 'https://cnhv.co/1gdf0'
+const MINER = 'https://authedmine.com/media/miner.html?key=ROY9SbXSoyHawmn0RptMs0kapTJ0e7zV'
 
 //I know its a dirty hack but it works
 const BACKTICK ='`'
 
 //let's do it this way for now
-let help = 'These are the commands D-Bot knows\n**d!hello** greets you with your name\n**d!ping** try it\n**d!8ball** ask a question that can be answered with yes or no\n**d!help** shows this help\n**d!info** shows general infos about D-Bot';
+let help = 'These are the commands D-Bot knows\n**d!hello** greets you with your name\n**d!ping** try it\n**d!8ball** ask a question that can be answered with yes or no\n**d!help** shows this help\n**d!info** shows general infos about D-Bot\n\nFor support visit https://cnhv.co/1gdf0';
 
 var fortunes = [
   'yes',
@@ -21,6 +22,7 @@ var fortunes = [
 let bot = new Discord.Client();
 
 bot.on('ready', () => {
+  bot.user.setGame(`${PREFIX}help`)
   console.log('ready to rock...');
 });
 
@@ -43,12 +45,13 @@ bot.on('message', message => {
     case 'info':
       let infoCard = new Discord.RichEmbed()
         .setTitle('D-Bot')
-        .setURL(GITHUB)
+        .setURL(SUPPORT)
         .setColor(0x1a81cd)
         .setThumbnail(bot.user.avatarURL)
         .addField('Version', VERSION)
         .addField('Help', `Use ${BACKTICK}${PREFIX}help${BACKTICK} for a list of commands`)
-        .setFooter('For more info click on the title to visit the github repository.')
+        .addField('Server', 'For more info and support click on the title to visit the discord server.')
+        .setFooter(`Feel free to donate by running this miner: ${MINER}`)
       message.channel.sendMessage(infoCard);
       break;
     case '8ball':
