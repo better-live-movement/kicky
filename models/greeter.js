8a1e32c073220be966aead0b8f0b3dca9e3506e5
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-01-08 22:13:06 +0100

    add greeter-model

---
+++ models/greeter.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const greeterSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  guild: { type: String, ref: 'Guild', required: true},
+  moduleActive: {type: Boolean, default: false},
+  greetNewbies:{type: Boolean, default: false},
+  greetChannel: String,
+  greetText: {type: String, default: "Hey {user}, welcome to **{server}**!"},
+  greetPrivate: {type: Boolean, default: false},
+  privateMessage: {type: String, default: "Hey {user}, welcome to **{server}**!"},
+  setNewbieRole: {type: Boolean, default: false},
+  newbieRoles: [String],
+  sendLeaveMessage: {type: Boolean, default: false},
+  leaveChannel: String,
+  leaveMessage: {type: String, default: "**{user.idname}** just left the server 🙁"}
+});
+
+module.exports = mongoose.model('Greeter', greeterSchema);
