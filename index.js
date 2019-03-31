8ba0bc87c6aa72ab5f7d4dff841aeff18557770b
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-03-31 23:03:56 +0200

    add informer

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const Discord = require('discord.js');
 const YTDL = require('ytdl-core');
 
+const Informer = require('./modules/Informer');
+
 const TOKEN = require('./token.js');
 const CONFIG = require('./config.json');
-const PREFIX = 'k!';
+
+//const PREFIX = 'b!';
+const PREFIX = CONFIG.prefix;
 const VERSION = '3.0.4';
-const INVITE = 'https://discordapp.com/api/oauth2/authorize?client_id=384572972851265538&permissions=8&scope=bot';
-const SUPPORT = 'https://discord.gg/6VpxTbY';
-const MINER = 'https://authedmine.com/media/miner.html?key=ROY9SbXSoyHawmn0RptMs0kapTJ0e7zV';
-const DASHBOARD = 'https://kicky-home.herokuapp.com/';
-
-//I know its a dirty hack but it works
-const BACKTICK ='`'
-
-//let's do it this way for now
-let help = 'These are the commands Kicky knows\n**k!hello** greets you with your name\n**k!ping** try it\n**k!8ball** ask a question that can be answered with yes or no\n**k!help** shows this help\n**k!info** shows general infos about Kicky\n**k!play <youtubelink>**kicky plays the song (you have to be in a voice channel)\n\nFor support visit https://discord.gg/6VpxTbY';
 
 function play(connection, message){
   var server = servers[message.guild.id];
@@ -97,9 +91,20 @@ bot.on('message', message => {
   console.log("----------------------------------------------------------------------------------------------------------------------------");
   console.log(message);
 
-  let args = message.content.substring(PREFIX.length).split(' ');
+  //let args = message.content.substring(PREFIX.length).split(' ');
 
-  switch(args[0].toLowerCase()) {
+  msgArray = message.content.substring(PREFIX.length).split(' ');
+  let module = msgArray[0].toLowerCase();
+  let command;
+  let args;
+  if(msgArray[1]) {
+    command = msgArray[1].toLowerCase;
+  }
+  if(msgArray[2]) {
+    args =msgArray.slice(2);
+  }
+  console.log(module);
+  switch(module) {
     case 'hello':
       message.channel.send('Hello ' + message.author.toString() + '!');
       break;
@@ -107,25 +112,12 @@ bot.on('message', message => {
       message.channel.send('pong!');
       break;
     case 'help':
-      message.channel.send(help);
-      break;
     case 'info':
-      let infoCard = new Discord.RichEmbed()
-        .setTitle('Kicky')
-        .setURL(SUPPORT)
-        .setColor(0x1a81cd)
-        .setThumbnail(bot.user.avatarURL)
-        .addField('Version', VERSION)
-        .addField('Help', `Use ${BACKTICK}${PREFIX}help${BACKTICK} for a list of commands`)
-        .addField('Invite', `[Invite](${INVITE}) the bot to your server.`)
-        .addField('Server', `Click [here](${SUPPORT}) to visit the discord server.`)
-        .addField('Website', `Click [here](${DASHBOARD}) to visit the Website.`)
-        .addField('donate', `Feel free to donate by running [this](${MINER}) miner.`)
-        .setFooter(`I had / have fun writing this bot. I hope you enjoy using it.`)
-      message.channel.send(infoCard);
+        let informer = new Informer(message, bot.user.avatarURL);
+        informer.respond();
       break;
     case '8ball':
-      if(args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+      if(msgArray[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
       else message.channel.send('what?!');
       break;
     case 'anno':
@@ -136,27 +128,27 @@ bot.on('message', message => {
       }
       break;
     case 'setrole':
-      if(args[1]){
+      if(msgArray[1]){
         console.log('setrole has arguments');
       }
       break;
     case 'removerole':
-      if(args[1] && args[2]){
-        let member = message.guild.members.find("displayName", args[1])
+      if(msgArray[1] && msgArray[2]){
+        let member = message.guild.members.find("displayName", msgArray[1])
         if(member){
-          let role = message.guild.roles.find("name", args[2])
+          let role = message.guild.roles.find("name", msgArray[2])
           if(role){
             member.removeRole(role);
             message.channel.send('role ' + role + ' removed from member ' + member.displayName);
           }
           else{
             message.channel.send('member not found');
-            console.log('member ', args[1], ' not found');
+            console.log('member ', msgArray[1], ' not found');
           }
         }
         else{
           message.channel.send('member not found');
-          console.log('member ', args[1], ' not found');
+          console.log('member ', msgArray[1], ' not found');
         }
       }
       else{
@@ -167,7 +159,7 @@ bot.on('message', message => {
       message.guild.roles.find("name", "noob").delete();
       break;
     case 'play':
-      if (!args[1]) {
+      if (!msgArray[1]) {
         message.channel.send('please provide a link!');
         return;
       }
@@ -177,7 +169,7 @@ bot.on('message', message => {
       }
       if (!servers[message.guild.id]) servers[message.guild.id] = {queue: []};
       var server = servers[message.guild.id];
-      server.queue.push(args[1]);
+      server.queue.push(msgArray[1]);
       if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(connection => {
         play(connection, message);
       });
---
+++ modules/Informer.js
@@ -0,0 +1,111 @@
+const Discord = require('discord.js');
+
+const CONFIG = require('../config.json');
+const VERSION = '3.0.2';
+
+const INVITE = 'https://discordapp.com/api/oauth2/authorize?client_id=384572972851265538&permissions=8&scope=bot';
+const SUPPORT = 'https://discord.gg/6VpxTbY';
+const MINER ='https://authedmine.com/media/miner.html?key=ROY9SbXSoyHawmn0RptMs0kapTJ0e7zV';
+const DASHBOARD = 'https://kicky-home.herokuapp.com/';
+//let's do it this way for now
+const AVATAR = 'https://cdn.discordapp.com/avatars/405516819672203274/57c31366b8622443bc0b776ffdaf779c.png?size=2048';
+
+//I know it's still a dirty hack but it works and I like it! So go fuck your self!! ;-)
+const BACKTICK ='`'
+
+//old help
+//let help = 'These are the commands Kicky knows\n**k!hello** greets you with your name\n**k!ping** try it\n**k!8ball** ask a question that can be answered with yes or no\n**k!help** shows this help\n**k!info** shows general infos about Kicky\n**k!play <youtubelink>**kicky plays the song (you have to be in a voice channel)\n\nFor support visit https://discord.gg/6VpxTbY';
+
+let infoCard = new Discord.RichEmbed()
+  .setTitle('Kicky')
+  .setURL(SUPPORT)
+  .setColor(0x1a81cd)
+  .setThumbnail(AVATAR)
+  .setDescription('kicky is a multipurpose discord bot.')
+  .addField('Version', VERSION)
+  .addField('Help', `Use ${BACKTICK}${CONFIG.prefix}help${BACKTICK} for a list of commands`)
+  .addField('Invite', `[Invite](${INVITE}) the bot to your server.`)
+  .addField('Server', `Click [here](${SUPPORT}) to visit the discord server.`)
+  .addField('Website', `Click [here](${DASHBOARD}) to visit the Website.`)
+  .addField('donate', `Feel free to donate by running [this](${MINER}) miner.`)
+  .setFooter(`I had / have fun writing this bot. I hope you enjoy using it.`)
+
+let generalHelp = new Discord.RichEmbed()
+  .setTitle('Help')
+  .setColor(0x1a81cd)
+  .setThumbnail(AVATAR)
+  .setDescription('This is the general help. Kicky is organised in modules and every module have its own help.')
+  .addField(`${CONFIG.prefix}help`, 'shows this help')
+  .addField(`${CONFIG.prefix}info`, 'shows general infos about Kicky')
+  .addField(`${CONFIG.prefix}help music`, 'shows help about music commands')
+  .addField(`${CONFIG.prefix}help fun`, 'shows help about some fun stuff')
+  .setFooter(`for support visit the discord server(${SUPPORT}).`)
+
+let funHelp = new Discord.RichEmbed()
+  .setTitle('Fun')
+  .setColor(0x1a81cd)
+  .setThumbnail(AVATAR)
+  .setDescription("this are some fun commands Kicky knows")
+  .addField(`${CONFIG.prefix}hello`,'greets you with your name')
+  .addField(`${CONFIG.prefix}ping`, 'try it ;-)')
+  .addField(`${CONFIG.prefix}8ball <your question>`, 'ask a question that can be answered with yes or no')
+  .setFooter(`for support visit the discord server(${SUPPORT}).`)
+
+  let musicHelp = new Discord.RichEmbed()
+  .setTitle('Music Help')
+  .setColor(0x1a81cd)
+  .setThumbnail(AVATAR)
+  .setDescription("this are the music commands kicky knows")
+  .addField(`${CONFIG.prefix}play <youtubelink>`,'kicky plays the song (you have to be in a voice channel)')
+  .setFooter(`for support visit the discord server(${SUPPORT}).`)
+
+
+var method = Informer.prototype;
+
+function Informer(message, avatar) {
+    this._guildId = message.guild.id
+    this._message = message;
+    this._avatar = avatar;
+    this._config ={};
+}
+
+method.info = function() {
+    return infoCard;
+};
+
+method.help = function() {
+    return generalHelp;
+};
+
+method.respond = function() {
+  msgArray = this._message.content.substring(CONFIG.prefix.length).split(' ');
+  console.log(CONFIG);
+  let cmd = msgArray[0].toLowerCase();
+  let args;
+  if(msgArray[1]) {
+    args = msgArray[1].toLowerCase();
+  }
+
+  switch(cmd.toLowerCase()) {
+    case 'info':
+      this._message.channel.send(infoCard);
+      break;
+    case 'help':
+      if (args == '' || args == null){
+        this._message.channel.send(generalHelp);
+      } else {
+        switch(args) {
+          case 'music':
+            this._message.channel.send(musicHelp);
+            break;
+          case 'fun':
+            this._message.channel.send(funHelp);
+            break;
+          default:  this._message.channel.send(`unknown args: ${args}`);
+        }
+      }
+      break;
+  }
+};
+
+module.exports = Informer;
