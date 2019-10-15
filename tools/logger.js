7b233b77009f8b7b744298bca05cf2cfc72fae76
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-10-15 08:39:52 +0200

    add type to logger

--- tools/logger.js
+++ tools/logger.js
@@ -3,9 +3,13 @@ function Logger(name = 'kicky') {
     this._name = name;
 }
 
-method.add = function(content, name = 'log', date = false) {
+method.add = function(content, name = 'log', date = false, type = 'info') {
   //Log 2 console 4 now
-  console.log(name, ' :', content);
+  if(type == error){
+    console.error(content);
+  } else {
+    console.log(name, ' :', content);
+  }
 }
 
 module.exports = Logger;
